7101ba3277cbaf5d8411372a7339a63b84a6f7bf
최지연 <jyc@joonggonara.co.kr>
2022-10-20 17:44:15 +0900

    기능 추가

---
+++ Diary/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Jiyeon Choi on 2022. 10. 20..
+//
+
+import Foundation
+
+struct Diary {
+  var uuidString: String
+  var title: String
+  var contents: String
+  var date: Date
+  var isStar: Bool
+}
